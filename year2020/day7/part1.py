05ec54f78232495dbd5bae20049b493773e0f5a1
Carl Oscar Aaro <hello@carloscar.com>
2020-12-07 18:56:01 +0100

    year 2020 - day 7

--- year2020/day7/part1.py
+++ year2020/day7/part1.py
@@ -1,18 +1,38 @@
-import functools
-import itertools
-import math
 import re
 
 from values import values
 
 
 async def run():
-    pass
+    bag_rules = {}
+    for bag_type, contents_str in values.match_rows(r"^([a-z]+ [a-z]+) bags contain (.*)[.]$"):
+        contents = {}
+        if contents_str != 'no other bags':
+            for c in contents_str.split(", "):
+                m = re.match(r"^([0-9]+) ([a-z]+ [a-z]+) bags?$", c)
+                contents[m.group(2)] = int(m.group(1))
+        bag_rules[bag_type] = contents
+
+    def can_hold_bag(this_bag_type, test_bag_type):
+        contents = bag_rules[test_bag_type]
+        if not contents:
+            return False
+        if this_bag_type in contents:
+            return True
+        return any([can_hold_bag(this_bag_type, bag_type) for bag_type in contents.keys()])
+
+    my_bag_type = "shiny gold"
+    for bag_type in bag_rules.keys():
+        if can_hold_bag(my_bag_type, bag_type):
+            values.counter += 1
+
+    return values.counter
 
 
 # [values.year]            (number)  2020
 # [values.day]             (number)  7
 # [values.part]            (number)  1
 # [values.input_filename]  (str)     ./year2020/day7/input
+# [values.counter]         (number)  235
 #
-# Result: ...
+# Result: 235
--- year2020/day7/part2.py
+++ year2020/day7/part2.py
@@ -1,18 +1,34 @@
-import functools
-import itertools
-import math
 import re
 
 from values import values
 
 
 async def run():
-    pass
+    bag_rules = {}
+    for bag_type, contents_str in values.match_rows(r"^([a-z]+ [a-z]+) bags contain (.*)[.]$"):
+        contents = {}
+        if contents_str != 'no other bags':
+            for c in contents_str.split(", "):
+                m = re.match(r"^([0-9]+) ([a-z]+ [a-z]+) bags?$", c)
+                contents[m.group(2)] = int(m.group(1))
+        bag_rules[bag_type] = contents
+
+    my_bag_type = "shiny gold"
+    bags_to_count = [my_bag_type]
+    while bags_to_count:
+        current_bag_type = bags_to_count.pop()
+        for bag_type, bag_count in bag_rules[current_bag_type].items():
+            values.counter += bag_count
+            for _ in range(0, bag_count):
+                bags_to_count.append(bag_type)
+
+    return values.counter
 
 
 # [values.year]            (number)  2020
 # [values.day]             (number)  7
 # [values.part]            (number)  2
 # [values.input_filename]  (str)     ./year2020/day7/input
+# [values.counter]         (number)  158493
 #
-# Result: ...
+# Result: 158493
