ef5142c9e3050ee6e40745844d7120ec9c38bd7a
Carl Oscar Aaro <hello@carloscar.com>
2022-12-04 14:09:11 +0100

    2022.4

---
+++ year2022/day4/part2.py
@@ -0,0 +1,22 @@
+from values import values
+
+
+async def run():
+    result = 0
+
+    for a1, a2, b1, b2 in values.match_rows(r"(\d+)-(\d+),(\d+)-(\d+)", (int, )):
+        ax = set(range(int(a1), int(a2) + 1))
+        bx = set(range(int(b1), int(b2) + 1))
+        overlap = ax & bx
+        if overlap:
+            result += 1
+
+    return result
+
+
+# [values.year]            (number)  2022
+# [values.day]             (number)  4
+# [values.part]            (number)  2
+# [values.input_filename]  (str)     ./year2022/day4/input
+#
+# Result: 911
