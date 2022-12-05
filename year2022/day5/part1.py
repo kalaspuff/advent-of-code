ba2123e469ed0264b7eaefc85b029a102f6882ab
Carl Oscar Aaro <hello@carloscar.com>
2022-12-05 10:48:05 +0100

    2022.5

---
+++ year2022/day5/part1.py
@@ -0,0 +1,37 @@
+import re
+
+from values import values
+
+
+async def run():
+    stacks = []
+
+    x = 0
+    for x, row in enumerate(values.rows):
+        if re.match(r"^[0-9 ]*$", row):
+            break
+
+        crates = [row[i+1] for i in range(0, len(row), 4)]
+        for i, c in enumerate(crates):
+            if len(stacks) <= i:
+                stacks.append([])
+            if c != " ":
+                stacks[i].insert(0, c)
+
+    for row in values.rows[x+2:]:
+        count, from_stack, to_stack = map(int, re.match(r"^move (\d+) from (\d+) to (\d+)$", row).groups())
+        from_stack -= 1
+        to_stack -= 1
+
+        stacks[to_stack] = stacks[to_stack] + list(reversed(stacks[from_stack][-count:]))
+        stacks[from_stack] = stacks[from_stack][0:-count]
+
+    return "".join([s[-1] for s in stacks])
+
+
+# [values.year]            (number)  2022
+# [values.day]             (number)  5
+# [values.part]            (number)  1
+# [values.input_filename]  (str)     ./year2022/day5/input
+#
+# Result: QGTHFZBHV
