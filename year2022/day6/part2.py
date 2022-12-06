6511c83441cc048e189fb3935a5ff940da9d64be
Carl Oscar Aaro <hello@carloscar.com>
2022-12-06 18:42:42 +0100

    2022.6

---
+++ year2022/day6/part2.py
@@ -0,0 +1,16 @@
+from values import values
+
+
+async def run():
+    count = 14
+    for i in range(len(values.input_)):
+        if len(set(list(values.input_[i:i + count]))) == count:
+            return i + count
+
+
+# [values.year]            (number)  2022
+# [values.day]             (number)  6
+# [values.part]            (number)  2
+# [values.input_filename]  (str)     ./year2022/day6/input
+#
+# Result: 2202
