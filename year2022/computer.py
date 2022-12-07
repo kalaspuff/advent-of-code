967d3d0cbeda61d52eeee3acdba1ab841e070222
Carl Oscar Aaro <hello@carloscar.com>
2022-12-07 17:14:14 +0100

    2022.7 computer

--- year2022/computer.py
+++ year2022/computer.py
@@ -1,4 +1,5 @@
 import re
+import sys
 from functools import reduce
 
 
@@ -11,6 +12,10 @@ class File:
     @property
     def path(self):
         return "/" + f"{self.parent.path}/{self.name}".strip("/")
+
+    @property
+    def filesystem(self):
+        return self.parent if isinstance(self.parent, Filesystem) else self.parent.filesystem
 
     def __str__(self):
         return f"file('{self.path}', size={self.size})"
@@ -27,25 +32,33 @@ class Directory:
         if name not in self.dirs:
             self.dirs[name] = Directory(self, name)
 
+    def assign_dir(self, name, dir):
+        if name not in self.dirs:
+            self.dirs[name] = dir
+
     def add_file(self, name, size):
         if name not in self.files:
             self.files[name] = File(self, name, size)
 
     @property
     def size(self):
-        return sum([dir.size for dir in self.dirs.values()]) + sum([file.size for file in self.files.values()])
+        return sum([dir.size for dir in self.dirs.values() if dir.filesystem == self.filesystem]) + sum([file.size for file in self.files.values() if file.filesystem == self.filesystem])
 
     @property
     def path(self):
         return "/" + f"{self.parent.path}/{self.name}".strip("/")
+
+    @property
+    def filesystem(self):
+        return self.parent if isinstance(self.parent, Filesystem) else self.parent.filesystem
 
     def __str__(self):
         return f"dir('{self.path}', size={self.size})"
 
 
 class Filesystem:
-    def __init__(self, name, mount, total_space):
-        self.name = name
+    def __init__(self, label, mount, total_space):
+        self.label = label
         self.mount = mount
         self.root = Directory(self, "")
         self.total_space = total_space
@@ -63,13 +76,111 @@ class Filesystem:
         return self.mount
 
     def __str__(self):
-        return f"fs('{self.name}', mount='{self.mount}', used={self.used_space}, avail={self.available_space})"
+        return f"fs('{self.mount}', used={self.used_space}, avail={self.available_space}, label='{self.label}')"
+
+
+class Command:
+    def __init__(self, cmd=None, args=(), cmdline=None, output=None):
+        self.echo = False
+        self.cmd = (cmd or "").strip()
+        self.args = args
+        self.output = output
+        if cmdline is not None and cmdline.encode() == b"":
+           self.cmd = "logout"
+           self.echo = True
+        if cmdline is not None:
+            m = re.match(r"([^ ]+)(?:\s+(.*))?$", cmdline)
+            if m:
+                self.args = [arg for arg in str(m.group(2) or "").strip().split(" ") if arg]
+                self.cmd = m.group(1).strip()
+                self.output = None
+
+    def execute(self, interface):
+        if self.cmd:
+            try:
+                func = getattr(self, f"cmd_{self.cmd}")
+            except AttributeError:
+                interface.out([f"-sh: {self.cmd}: command not found"])
+                return
+
+            if self.echo:
+                interface.out([self.cmd])
+
+            output = func(interface, self.args, self.output)
+            interface.out(output)
+            return output
+
+    def cmd_exit(self, interface, args, output=None):
+        interface.tty = False
+
+    def cmd_logout(self, interface, args, output=None):
+        interface.tty = False
+
+    def cmd_info(self, interface, args, output=None):
+        return interface.computer.get_info()
+
+    def cmd_cd(self, interface, args, output=None):
+        cpu = interface.computer
+        arg = args[0]
+        interface.cwd = cpu.get_dir(arg) if arg.startswith("/") else cpu.get_dir(f"{interface.cwd.path}/{arg}")
+
+    def cmd_ls(self, interface, args, output=None):
+        cpu = interface.computer
+
+        if output is not None:
+            for row in output:
+                m = re.match(r"^([^ ]+) (.+)$", row)
+                if m and m.group(1) == "dir":
+                    try:
+                        interface.cwd.assign_dir(m.group(2), cpu.get_dir(f"{interface.cwd.path}/{m.group(2)}"))
+                    except KeyError:
+                        interface.cwd.mkdir(m.group(2))
+                elif m:
+                    interface.cwd.add_file(name=m.group(2), size=int(m.group(1)))
+            return
+
+        result = []
+        objects = sorted(list(interface.cwd.dirs.values()) + list(interface.cwd.files.values()), key=lambda x: x.name)
+        for obj in objects:
+            if isinstance(obj, Directory):
+                result.append(f"dir {obj.name}")
+            elif isinstance(obj, File):
+                result.append(f"{obj.size} {obj.name}")
+        return result
+
+    @property
+    def arg(self):
+        return " ".join(self.args).strip()
+
+
+class Interface:
+    def __init__(self, computer, tty=True):
+        self.computer = computer
+        self.tty = tty
+        self.cwd = sorted(computer.filesystems.values(), key=lambda x: x.mount)[0].root
+        self.output = []
+
+    def connect(self, interactive=True):
+        self.tty = True
+        while self.tty and interactive:
+            sys.stdout.write("$ ")
+            sys.stdout.flush()
+            cmdline = sys.stdin.readline()
+            self.execute(Command(cmdline=cmdline))
+
+    def execute(self, command):
+        return command.execute(self)
+
+    def out(self, output):
+        self.output = output
+        if self.tty and output:
+            print("\n".join(output))
 
 
 class Computer:
     def __init__(self, name, filesystems):
         self.name = name
-        self.filesystems = {fs.name: fs for fs in filesystems}
+        self.filesystems = {fs.label: fs for fs in filesystems}
 
     def _get_path(self, path):
         return "/" + "/".join(reduce(lambda a, b: ((a + [b]) if b != ".." else a[:-1]) if b else a, [p for p in path.split("/") if p], []))
@@ -94,7 +205,7 @@ class Computer:
 
     def get_all_dirs(self):
         result = []
-        for fs in sorted(self.filesystems.values()):
+        for fs in sorted(self.filesystems.values(), key=lambda x: x.mount):
             dirs = [fs.root]
             while dirs:
                 cwd = dirs.pop()
@@ -103,52 +214,48 @@ class Computer:
                     dirs.append(dir)
         return result
 
-    def set_state_from_terminal_log(self, terminal_log):
-        cmd = ""
-        arg = ""
-        cwd = self.filesystems["root"].root
-
-        for row in terminal_log.split("\n"):
-            m = re.match(r"^[$] ([^ ]+)(?:\s+(.*))?$", row)
-            if m:
-                cmd = m.group(1)
-                arg = str(m.group(2) or "").strip()
-                if cmd == "cd":
-                    if arg.startswith("/"):
-                        cwd = self.get_dir(arg)
-                    else:
-                        cwd = self.get_dir(cwd.path + "/" + arg)
+    def get_info(self, clean=False):
+        result = []
+        result.append(f"⋗ {self}")
+        fs = sorted(self.filesystems.values(), key=lambda x: x.mount)[0]
+        dirs = [fs.root]
+        while dirs:
+            cwd = dirs.pop()
+            cwd_path = (cwd.path.lstrip(fs.path).rstrip("/") + "/").lstrip("/")
+            cwd_level = len(cwd_path.split("/")) - 1
+            if isinstance(cwd.parent, Filesystem):
+                result.append("  " * cwd_level + "➔ " + (f"{cwd.name}/" if clean else f"{cwd.parent}"))
             else:
-                if cmd == "ls":
-                    m = re.match(r"^dir (.+)$", row)
-                    if m:
-                        try:
-                            self.get_dir(cwd.path + "/" + m.group(1))
-                        except KeyError:
-                            cwd.mkdir(m.group(1))
-                        continue
+                result.append("  " * cwd_level + "∴ " + (f"{cwd.name}/" if clean else f"{cwd}"))
+            for file in sorted(cwd.files.values(), key=lambda d: d.path):
+                result.append("  " * (cwd_level + 1) + f"· " + (f"{file.name}" if clean else f"{file}"))
+            for dir in sorted(cwd.dirs.values(), key=lambda d: d.path, reverse=True):
+                dirs.append(dir)
+        result.append("")
+        return result
 
-                    m = re.match(r"^([0-9]+) (.+)$", row)
-                    if m:
-                        cwd.add_file(name=m.group(2), size=int(m.group(1)))
-                        continue
+    def get_commands_from_terminal_log(self, terminal_log):
+        groups = []
+        for row in terminal_log.split("\n"):
+            if row.startswith("$ "):
+                groups.append([])
+            groups[-1].append(row)
 
-    def print_info(self, clean=False):
-        print("")
-        print(f"⋗ {self}")
-        for fs in sorted(self.filesystems.values()):
-            print(f"  ➔ {fs}")
-            dirs = [fs.root]
-            while dirs:
-                cwd = dirs.pop()
-                cwd_path = (cwd.path.lstrip(fs.path).rstrip("/") + "/").lstrip("/")
-                cwd_level = len(cwd_path.split("/")) - 1
-                print("  " * cwd_level + "  ∴ " + (f"{cwd.name}/" if clean else f"{cwd}"))
-                for file in sorted(cwd.files.values(), key=lambda d: d.path):
-                    print("  " * (cwd_level + 1) + f"  · " + (f"{file.name}" if clean else f"{file}"))
-                for dir in sorted(cwd.dirs.values(), key=lambda d: d.path, reverse=True):
-                    dirs.append(dir)
-        print("")
+        commands = []
+        for group in groups:
+            m = re.match(r"^[$] ([^ ]+)(?:\s+(.*))?$", group[0])
+            if m:
+                args = [arg for arg in str(m.group(2) or "").strip().split(" ") if arg]
+                commands.append(Command(cmd=m.group(1), args=args, output=group[1:]))
+
+        return commands
+
+    def set_state_from_terminal_log(self, terminal_log):
+        commands = self.get_commands_from_terminal_log(terminal_log)
+        interface = Interface(self, False)
+
+        for command in commands:
+            interface.execute(command)
 
     def __str__(self):
-        return f"cpu('{self.name}')"
+        return f"computer('{self.name}')"
--- year2022/day7/part1.py
+++ year2022/day7/part1.py
@@ -1,9 +1,15 @@
+import sys
+
 from values import values
 from year2022.computer import Computer, Filesystem
+
 
 async def run():
     cpu = Computer("The Device", filesystems=[Filesystem("root", mount="/", total_space=70000000)])
     cpu.set_state_from_terminal_log(values.input_)
+
+    # from year2022.computer import Command, Interface
+    # Interface(cpu).execute(Command(cmdline="info"))
 
     return sum([dir.size for dir in cpu.get_all_dirs() if dir.size <= 100000])
 
