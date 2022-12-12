8d500c14b624fb5373ae9f816ac3ab8191f7a802
Carl Oscar Aaro <hello@carloscar.com>
2022-12-12 11:44:03 +0100

    2022.12

--- year2022/day12/part1.py
+++ year2022/day12/part1.py
@@ -9,16 +9,21 @@ async def run():
     move = lambda pos, delta: tuple((v + d for v, d in (zip(pos, delta))))
     elevation = lambda c: string.ascii_lowercase.index("a" if c == "S" else ("z" if c == "E" else c))
 
-    end = matrix.pos("E")[0]
-    start = matrix.pos("S")[0]
-    lowest_visited = {start: 0}
     paths = set()
 
+    end = matrix.pos("E")[0]
+    start = matrix.pos("S")[0]
+
+    lowest_visited = {start: 0}
     path_stack = [(elevation(matrix[start]), 0, (start,))]
 
     while path_stack:
         best_values = max(path_stack)
         current_elevation, path_len, path = path_stack.pop(path_stack.index(best_values))
+
+        if paths and max(paths)[1] > path_len:
+            continue
+
         current = path[-1]
 
         if current == end:
--- year2022/day12/part2.py
+++ year2022/day12/part2.py
@@ -9,22 +9,24 @@ async def run():
     move = lambda pos, delta: tuple((v + d for v, d in (zip(pos, delta))))
     elevation = lambda c: string.ascii_lowercase.index("a" if c == "S" else ("z" if c == "E" else c))
 
+    paths = set()
     end = matrix.pos("E")[0]
 
-    options = set()
     for start in matrix.pos("a") + [matrix.pos("S")[0]]:
         lowest_visited = {start: 0}
-        paths = set()
-
         path_stack = [(elevation(matrix[start]), 0, (start,))]
 
         while path_stack:
             best_values = max(path_stack)
             current_elevation, path_len, path = path_stack.pop(path_stack.index(best_values))
+
+            if paths and max(paths)[1] > path_len:
+                continue
+
             current = path[-1]
 
             if current == end:
-                if (not paths or max(paths)[0:2] < best_values[0:2]) and (not options or max(options)[0:2] < best_values[0:2]):
+                if not paths or max(paths)[0:2] < best_values[0:2]:
                     print("FOUND", -path_len)
 
                 paths.add(best_values)
@@ -44,10 +46,7 @@ async def run():
                         path_stack.append((new_elevation, -new_path_len, new_path))
                         lowest_visited[new_pos] = new_path_len
 
-        if paths:
-            options.add(max(paths))
-
-    current_elevation, path_len, path = max(options)
+    current_elevation, path_len, path = max(paths)
 
     return -path_len
 
