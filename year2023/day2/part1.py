e2ccd64fccc4ddd11811f925f9d05f39f8600587
Carl Oscar Aaro <hello@carloscar.com>
2023-12-01 07:35:12 +0100

    prep for day 2

---
+++ year2023/day2/part1.py
@@ -0,0 +1,25 @@
+import functools
+import itertools
+import math
+import re
+
+import helpers
+from matrix import Matrix
+from values import values
+
+
+async def run():
+    result = 0
+
+    for row in values.rows:
+        pass
+
+    return result
+
+
+# [values.year]            (number)  2023
+# [values.day]             (number)  2
+# [values.part]            (number)  1
+# [values.input_filename]  (str)     ./year2023/day2/input
+#
+# Result: ...
