e416f80719657932aa0915bc2cf20f783681df87
Carl Oscar Aaro <hello@carloscar.com>
2023-12-04 09:34:47 +0100

    prep for day 5

---
+++ year2023/day5/part2.py
@@ -0,0 +1,27 @@
+import functools
+import itertools
+import math
+import re
+from collections import deque
+
+import helpers
+from helpers import inverse, inverse_dict, manhattan_distance, multisplit, transform, transform_dict, tuple_add
+from matrix import Matrix
+from values import values
+
+
+async def run():
+    result = 0
+
+    for row in values.rows:
+        pass
+
+    return result
+
+
+# [values.year]            (number)  2023
+# [values.day]             (number)  0
+# [values.part]            (number)  2
+# [values.input_filename]  (str)     ./year2023/day0/input
+#
+# Result: ...
