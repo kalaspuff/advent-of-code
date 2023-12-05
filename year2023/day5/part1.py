a872071d6227a938364284f0d8c3e81909a3a04f
Carl Oscar Aaro <hello@carloscar.com>
2023-12-05 17:22:19 +0100

    2023.12.05

--- year2023/day5/part1.py
+++ year2023/day5/part1.py
@@ -1,61 +1,31 @@
-import functools
-import itertools
-import math
-import re
-from collections import deque
-
-import helpers
-from helpers import inverse, inverse_dict, manhattan_distance, multisplit, transform, transform_dict, tuple_add
-from matrix import Matrix
+from helpers import transform_tuple
 from values import values
-
-if False:
-    # potential copy-pasta / quick inspiration
-
-    # > regex (full match + transform)
-    # input: "Object 123: R 3 sX 8 9 | L a++ m nn X k"
-    # output: (123, ["R", "3", "sX", "8", "9"], ["L", "a++", "m", "nn", "X", "k"])
-    for object_id_, part_1_, part_2_ in values.match_rows(
-        r"^[^\d]*(\d+):\s*(.*)\s*[|]\s*(.*)\s*$", transform=(int, str.split, str.split)
-    ):
-        pass
-
-    # > regex (find all + transform)
-    # input: "1  -  1, 48, 83, 86, 17; 83, 86,  6, 31, 17,  9, 48, 53"
-    # output: (1, {1, 48, 17, 83, 86}, {6, 9, 48, 17, 83, 53, 86, 31})
-    parse_numbers_ = lambda v_: set(map(int, v_.split(", ")))
-    for single_, setpart_1_, setpart_2_ in values.findall_rows(
-        r"((?:\d+(?:,\s+|)?)+)", transform=(int, parse_numbers_, parse_numbers_)
-    ):
-        pass
-
-    # > multisplit
-    # input: "a, b, c; d, e, f; g, h, i"
-    # output: [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]
-    result_ = multisplit(values.input, ["; ", ", "])
-
-    # > inverse dict (flip key and value)
-    # input: ["7 red", "3 green", "8 blue"]
-    # output: {'red': 7, 'green': 3, 'blue': 8}
-    result_ = inverse_dict(map(str.split, values.rows), transform=(str, int))
-
-    # > add tuple elements (good for vectors, positions, coordinates)
-    result_ = tuple_add((1, 2), (-1, 0))  # (0, 2)
-    result_ = tuple_add((1, 2), (1, -1))  # (2, 1)
-
-    # > get nearby vectors (positions) in a grid
-    pos = (5, 6)
-    for mod in itertools.product([-1, 0, 1], [-1, 0, 1]):
-        pos_ = tuple_add(pos, mod)
 
 
 async def run():
-    result = 0
+    seeds = transform_tuple(values.rows[0].split()[1:], int)
 
-    for row in values.rows:
-        pass
+    maps = []
+    for row in values.rows[1:]:
+        if row.endswith("map:"):
+            maps.append([])
+        elif row:
+            map_entry = transform_tuple(row.split(), (int, int, int))
+            maps[-1].append(map_entry)
 
-    return result
+    location = float("inf")
+    for seed in seeds:
+        number = seed
+        for map_ in maps:
+            for destination, source, sourcelen in map_:
+                if number not in range(source, source + sourcelen):
+                    continue
+                number += destination - source
+                break
+
+        location = min(location, number)
+
+    return location
 
 
 # [values.year]            (number)  2023
@@ -63,4 +33,4 @@ async def run():
 # [values.part]            (number)  1
 # [values.input_filename]  (str)     ./year2023/day5/input
 #
-# Result: ...
+# Result: 196167384
--- year2023/day5/part2.py
+++ year2023/day5/part2.py
@@ -1,22 +1,52 @@
-import functools
-import itertools
-import math
-import re
-from collections import deque
-
-import helpers
-from helpers import inverse, inverse_dict, manhattan_distance, multisplit, transform, transform_dict, tuple_add
-from matrix import Matrix
+from helpers import transform_tuple
 from values import values
 
 
 async def run():
-    result = 0
+    seeds = transform_tuple(values.rows[0].split()[1:], int)
+    initial_seed_ranges = set([(seeds[i], seeds[i + 1], 0) for i in range(0, len(seeds), 2)])
+    maps = []
+    for row in values.rows[1:]:
+        if row.endswith("map:"):
+            maps.append([])
+        elif row:
+            map_entry = transform_tuple(row.split(), (int, int, int))
+            maps[-1].append(map_entry)
 
-    for row in values.rows:
-        pass
+    seed_ranges = initial_seed_ranges.copy()
+    for map_ in maps:
+        updated_ranges = set()
+        while seed_ranges:
+            seed_range = seed_ranges.pop()
+            if seed_range in updated_ranges:
+                continue
+            start, length, modifier = seed_range
+            start += modifier
+            end = start + length - 1
+            for destination, source, sourcelen in map_:
+                source_end = source + sourcelen - 1
+                start_max = max(start, source)
+                end_min = min(end, source_end)
 
-    return result
+                if source > end or source_end < start or min(end - start, source_end - source) < 0:
+                    continue
+
+                updated_ranges.add((start_max - modifier, end_min - start_max + 1, modifier + (destination - source)))
+
+                if start < source:
+                    seed_ranges.add((start - modifier, source - start, modifier))
+                if end > source_end:
+                    seed_ranges.add((source_end + 1 - modifier, end - source_end, modifier))
+
+                break
+            else:
+                updated_ranges.add(seed_range)
+
+        if updated_ranges:
+            seed_ranges = updated_ranges.copy()
+            updated_ranges = set()
+
+    return min([start + modifier for start, length, modifier in seed_ranges])
 
 
 # [values.year]            (number)  2023
@@ -24,4 +54,4 @@ async def run():
 # [values.part]            (number)  2
 # [values.input_filename]  (str)     ./year2023/day5/input
 #
-# Result: ...
+# Result: 125742456
