f50d419829d3264ba771bd60a18f1592a9724f56
Carl Oscar Aaro <hello@carloscar.com>
2023-12-08 20:03:53 +0100

    2023-08-08

---
+++ year2023/day8/part2.py
@@ -0,0 +1,35 @@
+import math
+
+from values import values
+
+
+async def run() -> int:
+    instructions = values.rows[0]
+    node_connections = {
+        node: connections for node, *connections in values.match_rows(r"([^\s]+) = [(]([^,]+), ([^)]+)[)]", start=2)
+    }
+
+    starting_nodes = [node for node in node_connections if node.endswith("A")]
+    instruction_length = len(instructions)
+    cycle_lengths = {instruction_length}
+
+    for starting_node in starting_nodes:
+        current_node = starting_node
+        steps = 0
+        visited_nodes = {}
+        while current_node not in visited_nodes:
+            visited_nodes[current_node] = steps
+            direction = 0 if instructions[steps % instruction_length] == "L" else 1
+            current_node = node_connections[current_node][direction]
+            steps += 1
+        cycle_lengths.add(steps - visited_nodes[current_node])
+
+    return math.prod(cycle_lengths)
+
+
+# [values.year]            (number)  2023
+# [values.day]             (number)  8
+# [values.part]            (number)  2
+# [values.input_filename]  (str)     ./year2023/day8/input
+#
+# Result: 18625484023687
