a58922b09566f69f92e3a9ee495b37cbf87a352c
Carl Oscar Aaro <hello@carloscar.com>
2024-12-01 06:41:24 +0100

    2024.2 prep

---
+++ year2024/day2/part2.py
@@ -0,0 +1,28 @@
+import functools
+import heapq
+import itertools
+import math
+import re
+from collections import Counter, deque
+from functools import cache, lru_cache, reduce
+from itertools import combinations, cycle, islice, permutations, product
+
+import helpers
+from values import values
+
+
+async def run() -> int:
+    result = 0
+
+    for row in values:
+        pass
+
+    return result
+
+
+# [values.year]            (number)  2024
+# [values.day]             (number)  2
+# [values.part]            (number)  2
+# [values.input_filename]  (str)     ./year2024/day2/input
+#
+# Result:
