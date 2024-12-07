aee6b0160c37f76949f1ecef3395d91bc2fc2fd1
Carl Oscar Aaro <hello@carloscar.com>
2024-12-07 13:07:25 +0100

    2024.7 simplified

--- year2024/day7/part2.py
+++ year2024/day7/part2.py
@@ -1,6 +1,5 @@
 import itertools
 
-from helpers import paired
 from values import values
 
 
@@ -9,27 +8,19 @@ async def run() -> int:
 
     for test_value, *numbers in values.ints():
         op_count = len(numbers) - 1
-        calculations = set()
         for operators in itertools.product(*(("+", "*", "||"),) * op_count):
-            calculation = list(map(str, numbers[:]))
-            for calc_idx, op in zip(range(1, len(numbers) * 2, 2), operators):
-                calculation.insert(calc_idx, op)
-
-            # calculations.add(eval("".join(calculation)))
-
-            value = int(calculation[0])
-            for op, next_value in paired(calculation[1:]):
+            value = numbers[0]
+            for op, next_value in zip(operators, numbers[1:]):
                 if op == "+":
-                    value += int(next_value)
+                    value += next_value
                 elif op == "*":
-                    value *= int(next_value)
+                    value *= next_value
                 elif op == "||":
                     value = int(str(value) + str(next_value))
 
-            calculations.add(value)
-
-        if test_value in calculations:
-            result += test_value
+            if value == test_value:
+                result += test_value
+                break
 
     return result
 
